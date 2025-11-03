e32638a6f93d511d38f21a231915551f940e1cb6
Krishna kumar Yadav <154981848+Krishkumar32@users.noreply.github.com>
2025-11-03 12:52:37 +0530

    Create 01_helloWorld.py

---
+++ 01_helloWorld.py
@@ -0,0 +1,62 @@
+print("Hello World")
+
+#print("Hello World") tells Python to show the text “Hello World” on the screen.
+
+#print → command to show message/output
+# ("Hello World") → the message/text we want to show
+# " " (quotes) → tells Python that it is text (string)
+
+✅ What is Python?
+Python is a popular, high-level programming language created by Guido van Rossum in 1991.
+It is simple, powerful, and widely used in technology.
+
+✅ Where is Python Used?
+Python is used for:
+Web development
+Software & application development
+Data science & big data
+Automation & scripting
+Machine learning & AI
+Database handling
+
+
+# ✅ What is Python?
+# Python is a popular, high-level programming language created by Guido van Rossum in 1991.
+# It is simple, powerful, and widely used in technology.
+
+# ✅ Where is Python Used?
+# Python is used for:
+# Web development
+# Software & application development
+# Data science & big data
+# Automation & scripting
+# Machine learning & AI
+# Database handling
+
+# ✅ What Can Python Do?
+
+# Python can:
+# Build websites and web apps
+# Automate tasks and workflows
+# Connect to and manage databases
+# Handle large data and perform math operations
+# Create prototypes quickly and build real products
+
+# ✅ Why Choose Python?
+
+# Works on Windows, Mac, Linux, etc.
+# Very simple, English-like syntax
+# Write less code compared to other languages
+# Fast to test and build programs (interpreted)
+# Supports procedural, OOP, and functional programming
+
+# ✅ Good to Know
+
+# We use Python 3 (latest major version)
+# You can code Python in editors like VS Code, PyCharm, Thonny, etc.
+
+# ✅ Python Syntax (Simple Explanation)
+
+# Python uses indentation (spaces) to define blocks of code
+# No semicolons needed — each line ends a command
+# Looks clean and easy to read, like English
